87a6439dff2ec08d11d7ae6e0ce7a43a8b365b62
弥超 <tony_mistark@michaodeAir.lan>
2015-09-07 20:31:44 +0800

    Initial Commit

---
+++ TomGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TomGame
+//
+//  Created by 弥超 on 15/9/7.
+//  Copyright (c) 2015年 弥超. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TomGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TomGame
+//
+//  Created by 弥超 on 15/9/7.
+//  Copyright (c) 2015年 弥超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
